3f7fffaa3e4cc954402660e12a3eb06795fa639d
Tian Zhou <tianzhou93@gmail.com>
2022-12-17 20:27:38 -0800

    Added a wrapper for movie list

--- src/App.js
+++ src/App.js
@@ -2,33 +2,12 @@ import React, { useState, useEffect, useCallback } from 'react';
 import MovieList from './components/MovieList';
 
 import './App.css';
-import tmdbService from './services/TmdbService';
+
+import Movies from './components/Movies';
 
 function App() {
-  const [movies, setMovies] = useState([]);
-
-  const fetchMoviesHandler = useCallback(async () => {
-    const data = await tmdbService.getRatedMovies();
-    const formattedMovies = data.map(movie => {
-      return {
-        id: movie.id,
-        title: movie.original_title,
-        releaseDate: movie.release_date
-      }
-    });
-    setMovies(formattedMovies);;
-  }, []);
-
-  useEffect(() => {
-    fetchMoviesHandler();
-  }, [fetchMoviesHandler]);
-
   return (
-    <React.Fragment>
-      <section>
-        <MovieList movies={movies} />
-      </section>
-    </React.Fragment>
+    <Movies />
   );
 }
 
